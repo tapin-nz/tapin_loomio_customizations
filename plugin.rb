714208b7bfca161394582bf9130c283ba24132cc
James Kiesel <james.kiesel@gmail.com>
2017-12-12 09:45:20 +1300

    Add useSubgroups feature

--- plugin.rb
+++ plugin.rb
@@ -4,6 +4,8 @@ module Plugins
       setup! :tapin_customizations do |plugin|
         plugin.enabled = true
         plugin.use_asset_directory 'angular/decorators'
+        plugin.extend_class(AppConfig) { group_features << 'useSubgroups' }
+        plugin.use_translations "config/locales", :tapin
       end
     end
   end
